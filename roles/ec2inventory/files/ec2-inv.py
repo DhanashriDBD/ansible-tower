0c456dca53ac7b34bcf4f222c10aa790c5e44f68
DhanashriDBD <dhanashri.d@gmail.com>
2020-08-18 16:33:39 +0530

    Update ec2-inv.py

--- roles/ec2inventory/files/ec2-inv.py
+++ roles/ec2inventory/files/ec2-inv.py
@@ -8,7 +8,7 @@ all_regions = ec2_cli.describe_regions()
 #print(all_regions['Regions'])
 
 #CSV creation and write data to CSV
-csv_ob=open("EC2-Inventory.csv","w")
+csv_ob=open("EC2-Inventory.csv","w",newline='')
 csv_w=csv.writer(csv_ob)
 csv_w.writerow(['Instance ID','Region','Instance Type','State','Private IP','Public IP','Vpc ID','Subnet ID','Account ID'])
 
